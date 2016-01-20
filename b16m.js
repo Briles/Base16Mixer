c43ed70aed91062bfbf241ea7c9b4491071737af
Brian <brian@dougreilly.com>
2016-01-20 12:51:07 -0500

    Adhere to Airbnb JavaScript style guide

--- b16m.js
+++ b16m.js
@@ -1,90 +1,100 @@
-'use strict';
+(function() {
+  'use strict';
 
-// Dependencies
-var fs = require('fs'),
-    path = require('path'),
-    tinycolor = require("tinycolor2"),
-    yaml = require('js-yaml');
+  // Dependencies
+  var fs = require('fs');
+  var path = require('path');
+  var tinycolor = require('tinycolor2');
+  var yaml = require('js-yaml');
 
-var inputSchemes = process.argv[2],
-    tempScheme = {
-        scheme: [],
-        author: [],
-        base00: [],
-        base01: [],
-        base02: [],
-        base03: [],
-        base04: [],
-        base05: [],
-        base06: [],
-        base07: [],
-        base08: [],
-        base09: [],
-        base0A: [],
-        base0B: [],
-        base0C: [],
-        base0D: [],
-        base0E: [],
-        base0F: []
-    },
-    mixedScheme = {};
+  // Utility Functions
+  function uniques(arr) {
+    var a = [];
+    for (var i = 0, l = arr.length; i < l; i++) {
+      if (a.indexOf(arr[i]) === -1 && arr[i] !== '') {
+        a.push(arr[i]);
+      }
+    }
 
-if (typeof(inputSchemes) === 'undefined') throw new Error('Usage: b16m <Scheme 1>,<Scheme 2>');
+    return a;
+  }
 
-inputSchemes = inputSchemes.split(',');
-var numSchemes = inputSchemes.length;
+  var inputSchemes = process.argv[2];
+  var tempScheme = {
+    scheme: [],
+    author: [],
+    base00: [],
+    base01: [],
+    base02: [],
+    base03: [],
+    base04: [],
+    base05: [],
+    base06: [],
+    base07: [],
+    base08: [],
+    base09: [],
+    base0A: [],
+    base0B: [],
+    base0C: [],
+    base0D: [],
+    base0E: [],
+    base0F: [],
+  };
+  var mixedScheme = {};
 
-if (numSchemes != 2) throw new Error('Missing ' + (2 - numSchemes) + ' inputSchemes to mix');
+  if (typeof (inputSchemes) === 'undefined') {
+    throw new Error('Usage: b16m <Scheme 1>,<Scheme 2>');
+  }
 
-inputSchemes.forEach(function(scheme)
-{
-    var contents = yaml.safeLoad(fs.readFileSync(path.resolve(scheme), 'utf8'), function(err)
-    {
-        if (err) return console.log(err);
+  inputSchemes = inputSchemes.split(',');
+  var numSchemes = inputSchemes.length;
+
+  if (numSchemes !== 2) {
+    throw new Error('Missing ' + (2 - numSchemes) + ' inputSchemes to mix');
+  }
+
+  inputSchemes.forEach(function(scheme) {
+    var raw = fs.readFileSync(path.resolve(scheme), 'utf8');
+
+    var contents = yaml.safeLoad(raw, function(err) {
+      if (err) {
+        return console.log(err);
+      }
     });
 
-    for (var prop in contents)
-    {
-        if (!contents.hasOwnProperty(prop)) continue;
+    for (var prop in contents) {
+      if (!contents.hasOwnProperty(prop)) {
+        continue;
+      }
 
-        if (tempScheme[prop] === undefined) throw new Error('"' + prop + '" not a valid Base16 scheme property');
-        tempScheme[prop].push(contents[prop]);
+      if (tempScheme[prop] === undefined) {
+        throw new Error('"' + prop + '" not a valid Base16 scheme property');
+      }
+
+      tempScheme[prop].push(contents[prop]);
     }
-});
+  });
 
-for (var prop in tempScheme)
-{
-    if (!tempScheme.hasOwnProperty(prop)) continue;
-
-    if (prop === 'scheme' || prop === 'author')
-    {
-        mixedScheme[prop] = uniques(tempScheme[prop]).join(' - ');
+  for (var prop in tempScheme) {
+    if (!tempScheme.hasOwnProperty(prop)) {
+      continue;
     }
-    else
-    {
-        mixedScheme[prop] = tinycolor.mix.apply(null, tempScheme[prop]).toHex();
-        console.log(tempScheme[prop][0] + ' + ' + tempScheme[prop][1] + ' --> ' + mixedScheme[prop]);
+
+    if (prop === 'scheme' || prop === 'author') {
+      mixedScheme[prop] = uniques(tempScheme[prop]).join(' - ');
+    } else {
+      mixedScheme[prop] = tinycolor.mix.apply(null, tempScheme[prop]).toHex();
+      console.log(tempScheme[prop][0] + ' + ' + tempScheme[prop][1] + ' --> ' + mixedScheme[prop]);
     }
-}
+  }
 
-var destPath = path.join(process.cwd(), mixedScheme.scheme + '.yml');
+  var destPath = path.join(process.cwd(), mixedScheme.scheme + '.yml');
 
-fs.writeFile(destPath, yaml.dump(mixedScheme), function(err)
-{
-    if (err) return console.log(err);
+  fs.writeFile(destPath, yaml.dump(mixedScheme), function(err) {
+    if (err) {
+      return console.log(err);
+    }
+
     console.log('Scheme saved to "' + destPath + '"');
-});
-
-// Utility Functions
-function uniques(arr)
-{
-    var a = [];
-    for (var i = 0, l = arr.length; i < l; i++)
-    {
-        if (a.indexOf(arr[i]) === -1 && arr[i] !== '')
-        {
-            a.push(arr[i]);
-        }
-    }
-    return a;
-}
+  });
+}());
